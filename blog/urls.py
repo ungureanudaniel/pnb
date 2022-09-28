ddd8a6c837207622ece9c99d6f2b4617d18dc833
ungureanudaniel <ungureanu.daniel86@gmail.com>
2022-09-28 17:35:52 +0300

    Create the main project and 4 apps: blog, services, tickets, users, setup in settings, credentials feed setup, installed django-debug-toolbar, ckeditor, captcha for forms, .gitignore

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import gettext_lazy as _
+
+urlpatterns = [
+    #------ general urls-------------------
+    path('i18n/', include('django.conf.urls.i18n')),
+    ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ pnb_main/urls.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.urls import path, include
+import debug_toolbar
+from django.conf import settings
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import ugettext_lazy as _
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('i18n/', include('django.conf.urls.i18n')),
+    ]
+urlpatterns += i18n_patterns(
+    path('users/', include('users.urls')),
+    path('', include('services.urls')),
+    path('', include('blog.urls')),
+    path('captcha/', include('captcha.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
+)+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
